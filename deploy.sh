649312a9a0921fc112d2c30ba8d28df73632112a
velmyk <velmyk@gmail.com>
2016-05-10 19:09:14 +0300

    fix(deploy) little fix

--- deploy.sh
+++ deploy.sh
@@ -37,7 +37,6 @@ doCompile
 cd target
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
-ls -la
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if [ -z `git diff --exit-code` ]; then
@@ -55,7 +54,10 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+ls -la
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out deploy_key -d
+ls -la
+pwd
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
