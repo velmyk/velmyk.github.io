d4c12a18974b77cadef0d36e79fa5c01d4a3b01b
velmyk <velmyk@gmail.com>
2016-05-11 11:34:36 +0300

    feature(deploy) text

--- deploy.sh
+++ deploy.sh
@@ -55,6 +55,7 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 ls -la
+ssh-add -l
 openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out deploy_key -d
 ls -la
 pwd
